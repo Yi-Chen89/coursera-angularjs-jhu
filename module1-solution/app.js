5d619e3a315a0dca05ca44818b73137ca8b7923a
YChen89 <Yi.Chen@arup.com>
2022-01-13 11:15:39 -0500

    Complete Module 1 Assignment

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,11 +8,25 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
     $scope.lunch = '';
 
-    $scope.lunchCount = function ($scope.lunch) {
-        lunches = $scope.lunch.split(',');
-        return lunches.length;
+    function msgProducer () {
+        if ($scope.lunchNum === 0) {
+            $scope.lunchMsg = 'Please enter data first';
+        } else if ($scope.lunchNum <= 3) {
+            $scope.lunchMsg = 'Enjoy!';
+        } else {
+            $scope.lunchMsg = 'Too much!';
+        }
     };
 
+    $scope.lunchCount = function () {
+        if ($scope.lunch === '') {
+            $scope.lunchNum = 0;
+        } else {
+            var lunches = $scope.lunch.split(',');
+            $scope.lunchNum = lunches.length;
+        }
+        msgProducer();
+    };
     
 };
 
